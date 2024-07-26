396c1a1f807b0b1acb4875d98568100b52517a25
vickyTyloones <vickykumarworkmail@gmail.com>
2024-07-27 00:36:43 +0530

    prisma orm added

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const express = require("express");
 require("dotenv").config();
 const app = express();
-const sequelize = require("./db_connection/db");
-const connect = require("./db_connection/connection");
+
 const postRouter = require("./router/postRouter");
 const PORT = process.env.PORT || 3000;
 
@@ -11,11 +10,10 @@ app.use(express.json());
 //routers
 app.use("/api", postRouter);
 
-sequelize.sync().then(() => {
+
   app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
-    connect();
   });
-});
+
 
 //sync is not suggested in production
--- controler/postControler.js
+++ controler/postControler.js
@@ -1,33 +1,45 @@
-const Post = require('../models/post')
+const prisma = require("../db_connection/db.config");
 
 
-const createPost =  async (req, res) => {
-    try {
-        const { title, content } = req.body;
-        const newPost = await Post.create({ title, content });
-        res.status(201).json({
-          success: true,
-          data: newPost,
-          message: "post created successfully",
-        });
-      } catch (error) {
-        console.error("Error creating post:", error);
-        res.status(500).json({ error: "Failed to create post" });
-      }
-}
-
+const createPost = async (req, res) => {
+  try {
+    const { name, email } = req.body;
+    const finduser = await prisma.user.findUnique({
+      where: {
+        email: email,
+      },
+    });
+    if (finduser) {
+      return res.json({ status: 400, message: "email already taken" });
+    }
+    const newUser = await prisma.user.create({
+      data: {
+        name: name,
+        email: email,
+      },
+    });
+    return res.status(201).json({
+      success: true,
+      data: newUser,
+      message: "post created successfully",
+    });
+  } catch (error) {
+    console.error("Error creating post:", error);
+    res.status(500).json({ error: "Failed to create post" });
+  }
+};
 
 const getAllPost = async (req, res) => {
-    try {
-        const posts = await Post.findAll();
-        res.json({
-          success: true,
-          data: posts,
-          message: "data retrive successfully",
-        });
-      } catch (error) {
-        console.error("Error fetching posts:", error);
-        res.status(500).json({ error: "Failed to fetch posts" });
-      }
-}
-module.exports = {createPost, getAllPost}
+  try {
+    const posts = await Post.findAll();
+    res.json({
+      success: true,
+      data: posts,
+      message: "data retrive successfully",
+    });
+  } catch (error) {
+    console.error("Error fetching posts:", error);
+    res.status(500).json({ error: "Failed to fetch posts" });
+  }
+};
+module.exports = { createPost, getAllPost };
