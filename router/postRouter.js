f2d9c7928171b2eaf422090467bbdffc22ef8a00
vickyTyloones <vickykumarworkmail@gmail.com>
2024-07-30 22:58:34 +0530

    get all users api created

--- router/postRouter.js
+++ router/postRouter.js
@@ -1,11 +1,16 @@
 const router = require("express").Router();
-const {createPost, getAllPost} = require('../controler/postControler')
+const {
+  createUser,
+  getAllUser,
+  getUserById
+} = require("../controler/postControler");
 
 // Route to create a new post
-router.route("/posts").post(createPost);
-
+router.route("/user").post(createUser);
 
 //Route to get all post
-router.route("/posts/:id").get(getAllPost)
+router.route("/user/:id").get(getUserById);
+
+router.route('/users').get(getAllUser)
 
 module.exports = router;
